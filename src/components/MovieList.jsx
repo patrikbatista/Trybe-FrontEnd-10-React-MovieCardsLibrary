bbfa46b2b4d30caba4c7887b7b0b2d2676461466
Patrik Batista <pcr_batista@homail.com>
2021-09-02 10:48:10 -0300

    finaliza projeto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,33 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((movie, index) => {
+          const fraseKey = `Movie Title ${index + 1}`;
+          return <MovieCard movie={ movie } key={ fraseKey } />;
+        })}
+      </div>
+    );
+  }
+}
+
+// MovieList.defaultProps = {
+//   movies: [{
+//     title: '',
+//     subtitle: '',
+//     storyline: '',
+//     rating: 0,
+//     imagePath: '',
+//   }],
+// };
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+export default MovieList;
